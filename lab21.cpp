4dc2afe9f598fc707051bb1e02794f78422c5752
majespectre00 <oat601_12@hotmail.com>
2021-03-09 17:23:53 +0700

    lab21 completed

--- lab21.cpp
+++ lab21.cpp
@@ -27,7 +27,48 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
-
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real*c.real)-(imag*c.imag),(real*c.imag)+(imag*c.real));
+}
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber(((real*c.real)+(imag*c.imag))/(pow(c.real,2)+pow(c.imag,2)),((-real*c.imag)+(imag*c.real))/(pow(c.real,2)+pow(c.imag,2)));
+}
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag) return true;
+	else return false;
+}
+double ComplexNumber::abs(){	
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+double ComplexNumber::angle(){
+	return atan2(imag,real)*180/M_PI;
+}
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.real == 0 && c.imag == 0) return os <<"0";
+	else if(c.real != 0 && c.imag > 0) return os <<c.real<<"+"<<c.imag<<"i";
+	else if(c.real != 0 && c.imag <0) return os <<c.real<<c.imag<<"i";
+	else if(c.real != 0 && c.imag == 0) return os <<c.real;
+	else return os <<c.imag<<"i";
+	
+}
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+ComplexNumber operator/(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real/(pow(c.real,2)+pow(c.imag,2)),s*-c.imag/(pow(c.real,2)+pow(c.imag,2)));
+}
+bool operator==(double s,const ComplexNumber &c){
+	if(c.imag == 0 && s == c.real){
+		return true;
+	}
+	else return false;
+}
 //Write your code here
 
 int main(){
